94fc0c25d84e140ae8ec1d28e92c6707145dc88c
Yohanes Demissie <Yohanesdemissie@gmail.com>
2017-12-20 11:09:45 -0800

    update api url on client

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,42 +1,33 @@
-'user strict';
+'use strict'
 
-var app = app || {}; //semicolon necessary so it isnn't a run on to the next IFFEE line of code. 
+var app = app || {};
 
 (function(module) {
-    var __API_URL__ = 'http://localhost:3000'; //in the IFFEE so the local host doesnt show to the client
+//   var __API_URL__ = 'http://localhost:3000'; //https://gn-yd-booklist.herokuapp.com/
+  var __API_URL__ = 'https://gn-yd-booklist.herokuapp.com/';
 
-    //ERROR function for the client to see
-    function errorCallBack(err) {
-        cjonsole.error(err)
-        module.errorView.initErrorPage(err) 
-    }
+  function errorCallBack(err) {
+    console.error(err)
+    module.errorView.initErrorPage(err)
+  }
 
-    function Book(RawBookObj) {
-        Object.keys(rawBookObj).map(key => this[key] = rawBookObj[key]) //this creates a property for each book instance
-    }
+  function Book(rawBookObj) {
+    Object.keys(rawBookObj).map(key => this[key] = rawBookObj[key])
+  }
 
-    Book.prototype.toHtml = function () {
-           // let template = Handlebars.compile($('#book-list-template').text())
-           // return template(this)
-        return Handlebars.compile($('#book-list-template').text())(this)
-    }
+  Book.prototype.toHtml = function() {
+    // let template = Handlebars.compile($('#book-list-template').text())
+    // return template(this)
+    return Handlebars.compile($('#book-list-template').text())(this)
+  }
 
-    Book.all = []
-    //this take an array of book objects to book.all and create an object constructor and create new instance by book title
-    Book.loadAll = rows => Book.all = rows.sort((a, b) => a.title - b.title).map(book => new Book(book))
-    Book.fetchAll = callback =>
-      $.get(`${__API_URL__}/api/v1/books`)
-      .then(Book.loadAll)
-      .then(callback)
-      .catch(errorCallback) //continue the console log off error from above
+  Book.all = []
+  Book.loadAll = rows => Book.all = rows.sort((a, b) => a.title - b.title).map(book => new Book(book))
+  Book.fetchAll = callback =>
+    $.get(`${__API_URL__}/api/v1/books`)
+    .then(Book.loadAll)
+    .then(callback)
+    .catch(errorCallBack)
 
-
-    module.Book = Book
+  module.Book = Book
 })(app)
-
-//TEST IN SERVER WITHOUT FRONT END
-//api.jquery.com
-//$.get(http://localhost:3000/avi/vi1/books')
-//.then(console.log)
-//TIMESTAMP 3:15 for 
-//terminal: curl "http url thing like above paste here no postophies" and will run your back end work
