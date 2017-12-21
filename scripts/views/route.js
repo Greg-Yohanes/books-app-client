844e2ecb9cbefcba608c533b82f3ceba986088e8
Your Name <nords3x4@gmail.com>
2017-12-21 13:57:15 -0800

    debugging rendering details page

--- scripts/views/route.js
+++ scripts/views/route.js
@@ -2,9 +2,8 @@
 
 page('/', () => app.Book.fetchAll(app.bookView.initIndexView))
 page('/books/:id', ctx => {
-  console.log(ctx)
+  console.log(ctx);
   app.Book.fetchOne(ctx.params.id, app.bookView.initDetailPage)})
 page() //call back for the a'bove code
 
 //Part two crazy load of info!! ! !! ! !! ! !!
-
