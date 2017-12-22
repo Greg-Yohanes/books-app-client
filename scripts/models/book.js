2e5f5ace1380164fb301f113ed2ded05877c6404
Your Name <nords3x4@gmail.com>
2017-12-22 11:06:23 -0800

    Trying a new html format for the inputs

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -40,11 +40,41 @@ var app = app || {};
     .catch(errorCallBack)
 
   
-  Book.prototype.insertRecord = function(callback) {
-    $.post(`${__API_URL__}`, {author: this.author, image_url: this.image_url, body: this.description, isbn: this.isbn, title: this.title})
-   .then(console.log)
-   .then(callback);
-};
+//   Book.prototype.insertRecord = function(callback) {
+//     $.post(`${__API_URL__}`, {author: this.author, image_url: this.image_url, body: this.description, isbn: this.isbn, title: this.title})
+//    .then(console.log)
+//    .then(callback);
+// };
+
+Book.create = function(book) {
+
+  $.post(`${__API_URL__}/api/v1/books/new`,book)
+    // .then(console.log('record inserted'))
+    .catch(errorCallback)
+}
+//searches Google Books api for matches
+Book.find = function(book, callback) {
+  //console.log('book', book)
+
+  $.get(`${__API_URL__}/api/v1/books/find`,book)
+    .then(data => {
+    //  console.log('find data', data)
+      Book.loadAll(data);
+    })
+    .then(callback)
+    .catch(errorCallback)
+}
+
+Book.delete = function(ctx) {
+  $.ajax({
+    url: `${__API_URL__}/api/v1/books/delete/${ctx.params.id}`,
+    method: 'DELETE'
+  })
+    // .then(data => {
+    //   console.log(data)
+    // })
+    .then(() => page('/'))
+}
 
   module.Book = Book;
 })(app)
--- scripts/views/route.js
+++ scripts/views/route.js
@@ -1,10 +1,13 @@
 'use strict';
 
 page('/', () => app.Book.fetchAll(app.bookView.initIndexView))
+
 page('/books/:id', ctx => {
   console.log(ctx);
   app.Book.fetchOne(ctx.params.id, app.bookView.initDetailPage)})
-page('/newbook',app.bookView.initNewBook)
+
+page('/newbook', app.bookView.initNewBook)
+
 page() //call back for the a'bove code
 
 //Part two crazy load of info!! ! !! ! !! ! !!
